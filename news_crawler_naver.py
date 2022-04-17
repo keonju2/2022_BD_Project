1267cfeeb817b22a4986eebc52a901994fd9be39
주건나 <54880474+keonju2@users.noreply.github.com>
2022-04-18 02:12:57 +0900

    Add files via upload

--- news_crawler_naver.py
+++ news_crawler_naver.py
@@ -7,13 +7,16 @@ from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import re
 import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common import exceptions
+
 
 pd.set_option('display.max_columns', None)
-s = Service('chromedriver.exe')
+s = Service(ChromeDriverManager().install())
 
 
 def crawler(company,date_1,date_2,max_page):
-    total_result = pd.DataFrame(columns={'Company','Title','Date','Source','Link','Article'})
+    total_results = pd.DataFrame(columns={'Company','Title','Date','Source','Link'})
 
     for page in range(int(max_page)):
         driver = webdriver.Chrome(service=s)
@@ -27,7 +30,7 @@ def crawler(company,date_1,date_2,max_page):
         date_list = []
         source_list = []
         link_list = []
-
+        company_list = []
         news_info = bs0bj.find_all('div','news_area')
 
         for i in range(len(news_info)):
@@ -47,52 +50,36 @@ def crawler(company,date_1,date_2,max_page):
 
             source = '매일경제'
             source_list.append(source)
+            company_list.append(company)
+        total = {'Company':company_list,'Title':title_list,'Date':date_list,'Source':source_list,'Link':link_list}
+        total = pd.DataFrame(total)
+        total_results = total_results.append(total,ignore_index=True)
 
-        for i in range(len(title_list)):
-            total = {'Company':company,'Title':title_list[i],'Date':date_list[i],'Source':source_list[i],'Link':link_list[i],'Article':None}
-            driver.get(link_list[i])
-            html = driver.page_source
-            bs0bj = BeautifulSoup(html,'lxml')
-            time.sleep(3)
-            try:
-                article = bs0bj.find('div',class_='art_txt').text
-                article = re.sub('\n','',article)
-                article = re.sub('\t', '', article)
-                total['Article'] = article
 
-            except:
-                article = bs0bj.find('div', class_='read_txt').text
-                article = re.sub('\n', '', article)
-                article = re.sub('\t', '', article)
-                total['Article'] = article
-
-            total_result = total_result.append(total,ignore_index=True)
 
 
         driver.quit()
 
-    return total_result
+    return total_results
+
+
 
 finals = pd.DataFrame()
 
-for k in range(1,276):
-    final = crawler('네이버','2021.01.01','2021.06.30',str(k))
-    pd.concat([finals,final],ignore_index=True)
+final = crawler('네이버','2021.01.01','2021.06.30','276')
+finals = pd.concat([finals,final],ignore_index=True)
 
-for l in range(1,197):
-    final = crawler('네이버','2021.07.01','2021.12.31',str(k))
-    pd.concat([finals,final],ignore_index=True)
+final = crawler('네이버','2021.07.01','2021.12.31','197')
+finals = pd.concat([finals,final],ignore_index=True)
 
 finals.to_csv('네이버_1년.csv',index=False,encoding='utf-8')
 
 
 finals = pd.DataFrame()
-for k in range(1,234):
-    final = crawler('카카오','2021.01.01','2021.6.30',str(k))
-    pd.concat([finals,final],ignore_index=True)
+final = crawler('카카오','2021.01.01','2021.06.30','234')
+pd.concat([finals,final],ignore_index=True)
 
-for l in range(1,250):
-    final = crawler('카카오','2021.07.01','2021.12.31',str(k))
-    pd.concat([finals,final],ignore_index=True)
+final = crawler('카카오','2021.07.01','2021.12.31','250')
+pd.concat([finals,final],ignore_index=True)
 
 finals.to_csv('카카오_1년.csv',index=False,encoding='utf-8')
